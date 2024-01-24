0c0f5a926ed37de2883f2aa5f2abdae2e38d6e6d
RacoolStudi0 <119780307+RacoolStudi0@users.noreply.github.com>
2024-01-24 21:37:03 +0400

    main.c
    first time

--- main.c
+++ main.c
@@ -3,7 +3,7 @@ int main()
   int i;
   for (i = 0; i < 32; i++)
   {
-    printf("Hello School. This is Balash, it is first time for me\n");
+    printf("Hello School, it is balash\n");
   }
   return 0;
 }
